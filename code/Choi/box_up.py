06b65d2273755c7ba1179594d2f45f933b6a3671
vkie39 <107172953+vkie39@users.noreply.github.com>
2024-06-18 17:28:19 +0900

    동영상_저장_코드_추가_최서현

--- code/Choi/box_up.py
+++ code/Choi/box_up.py
@@ -19,6 +19,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--source', type=str, default="D:/대외 활동 및 프로젝트/YOLO를 활용한 자율주행/git_clone/Tickie_YOLOv5_Accuracy/sampleVideo/kickboard.mp4", help='file/dir/URL/glob')
 parser.add_argument('--weights', type=str, default="D:/대외 활동 및 프로젝트/YOLO를 활용한 자율주행/git_clone/Tickie_YOLOv5_Accuracy/code/Choi/model/best.pt", help='weights path')
 parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+parser.add_argument('--output', type=str, default='"D:/대외 활동 및 프로젝트/YOLO를 활용한 자율주행/git_clone/Tickie_YOLOv5_Accuracy/code/Choi/output.mp4', help='output video file')
 opt = parser.parse_args()
 
 # 디바이스 설정
@@ -32,6 +33,10 @@ class_names = model.module.names if hasattr(model, 'module') else model.names
 
 # 비디오 캡처
 cap = cv2.VideoCapture(opt.source)
+
+# 비디오 라이터 초기화
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter(opt.output, fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
 
 # 타임스탬프 초기화
 start_time = time.time()
@@ -77,21 +82,23 @@ while cap.isOpened():
 
         # 바운딩 박스를 확장하기 위해 좌표 조정
         bbox_extension = 10  # 확장할 크기 (픽셀 단위)
-        xyxy[0] = max(xyxy[0] - bbox_extension, 200)  # x_min
-        xyxy[1] = max(xyxy[1] - bbox_extension, 500)  # y_min
+        xyxy[0] = max(xyxy[0] - bbox_extension, 0)  # x_min
+        xyxy[1] = max(xyxy[1] - bbox_extension, 0)  # y_min
         xyxy[2] = min(xyxy[2] + bbox_extension, w)  # x_max
         xyxy[3] = min(xyxy[3] + bbox_extension, h)  # y_max
 
         label = f'{class_names[int(cls)]} {conf:.2f}'
-        cv2.rectangle(frame, (xyxy[0], xyxy[1] - 10), (xyxy[2], xyxy[3] + 20), (255, 0, 0), 2)
-        cv2.putText(frame, label, (xyxy[0], xyxy[1] - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+        cv2.rectangle(frame, (xyxy[0], xyxy[1]), (xyxy[2], xyxy[3]), (255, 0, 0), 2)
+        cv2.putText(frame, label, (xyxy[0], xyxy[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
         print(f"Class: {class_name}, Confidence: {conf:.2f}, BBox: {xyxy}")
 
     cv2.imshow('YOLOv5 Detection', frame)
+    out.write(frame)  # 프레임 저장
     
     if cv2.waitKey(1) & 0xFF == ord('q'):  # 'q' 키를 눌러 종료
         break
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()
